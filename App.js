d23dc66d5ef524009777d0d707c7080f7b275eaf
jonhayjr <jonhayjr@gmail.com>
2021-08-26 18:43:11 -0700

    Initialized files

--- App.js
+++ App.js
@@ -1,21 +1,247 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// Basic reusable components
+
+const Avatar = (props) => (
+  <Image
+    style={styles.avatar}
+    source={{ uri: props.url }}
+  />
+);
+
+const Heading = (props) => (
+  <Text style={styles.heading}>
+    {props.children}
+  </Text>
+);
+
+const Title = (props) => (
+  <Text style={styles.title}>
+    {props.children}
+  </Text>
+);
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  avatar: {
+    borderRadius: 50,
+    width: 45,
+    height: 45,
+    marginTop: 2
+  },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    margin: 5
+  },
+  title: {
+    fontSize: 12,
+    textTransform: 'uppercase',
+    fontWeight: 'bold'
   },
 });
+
+// App-specific components
+
+const WoofCard = (props) => (
+  <View style={woofCardStyles.card}>
+  <Avatar url={props.avatar}/>
+    <View style={{textAlign: 'center', marginTop: 7}}>
+      <Title>{props.name}</Title>
+    </View>
+  </View>
+);
+
+const woofCardStyles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+    margin: 7,
+    width: 75,
+    borderColor: 'lightgray',
+    borderStyle: 'solid',
+    borderWidth: 2,
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+    shadowColor: "#000",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4
+  }
+});
+
+const WoofPost = (props) => (
+  <View style={woofPostStyles.layout}>
+    <Image style={woofPostStyles.image} source={{ uri: props.image }}/>
+    <View style={woofPostStyles.content}>
+      <Text style={woofPostStyles.title}>{props.title}</Text>
+      <Text style={woofPostStyles.description} numberOfLines={2}>{props.description}</Text>
+    </View>
+  </View>
+);
+
+const woofPostStyles = StyleSheet.create({
+  layout: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 5
+  },
+  image: {
+    borderRadius: 10,
+    width: 100,
+    height: 75,
+    margin: 7,
+    flex: 1
+  },
+  content: {
+    margin: 3,
+    flex: 2
+  },
+  title: {
+   textTransform: 'uppercase',
+   fontSize: 12,
+   fontWeight: 'bold',
+   marginBottom: 2
+  },
+  description: {
+    fontSize: 10
+  },
+});
+
+
+// The screen rendering everything
+const HomeScreen = () => (
+  
+  <ScrollView>
+    <Heading>Trending Woofs</Heading>
+    <ScrollView>
+     <ScrollView style={{flex: 1, flexDirection: 'row'}} horizontal>
+    {
+      data.woofs.map((woof) => (
+         <WoofCard
+          key={woof.id}
+          name={woof.name}
+          avatar={woof.avatar}
+          />
+      ))
+    }
+    </ScrollView>
+    <Heading>New Woof Posts</Heading>
+       {
+      data.posts.map((post) => (
+         <WoofPost
+          key={post.id}
+          image={post.image}
+          title={post.title}
+          description={post.description}
+          />
+      ))
+    }
+    </ScrollView>
+  </ScrollView>
+);
+
+const App = () => (
+  <SafeAreaView style={{ flex: 1, backgroundColor: '#FAF9FA' }}>
+    <HomeScreen />
+  </SafeAreaView>
+);
+
+export default App;
+
+// "Fake" API data to use in your app
+const data = {
+  woofs: [
+    {
+      id: 'woof-1', 
+      name: 'Rex', 
+      avatar: 'https://images.unsplash.com/photo-1558788353-f76d92427f16?auto=format&fit=crop&w=648&q=80',
+      caretaker: 'Victor Grabarczyk',
+      source: 'https://unsplash.com/photos/x5oPmHmY3kQ',
+    },
+    {
+      id: 'woof-2', 
+      name: 'Ball', 
+      avatar: 'https://images.unsplash.com/photo-1585584114963-503344a119b0?auto=format&fit=crop&h=64&q=80',
+      caretaker: 'Tatiana Rodriguez',
+      source: 'https://unsplash.com/photos/J40C1k6Fut0',
+    },
+    {
+      id: 'woof-3', 
+      name: 'Happy', 
+      avatar: 'https://images.unsplash.com/photo-1543466835-00a7907e9de1?auto=format&fit=crop&h=64&q=80',
+      caretaker: 'Marliese Streefland',
+      source: 'https://unsplash.com/photos/2l0CWTpcChI',
+    },
+    {
+      id: 'woof-4',
+      name: 'Fluffy',
+      avatar: 'https://images.unsplash.com/photo-1554956615-1ba6dc39921b?auto=format&fit=crop&h=64&q=80',
+      caretaker: 'Nick Fewings',
+      source: 'https://unsplash.com/photos/rMKXLAIa2OY',
+    },
+    {
+      id: 'woof-5',
+      name: 'Spirit',
+      avatar: 'https://images.unsplash.com/photo-1514984879728-be0aff75a6e8?auto=format&fit=crop&h=64&q=80',
+      caretaker: 'Jamie Street',
+      source: 'https://unsplash.com/photos/uNNCs5kL70Q',
+    },
+  ],
+  posts: [
+    {
+      id: 'post-1',
+      image: 'https://images.unsplash.com/photo-1544568100-847a948585b9?auto=format&fit=crop&w=967&q=80',
+      title: 'Happy Woofs',
+      description: 'How to keep your woof health and happy. We\'ve asked some of the best experts out there.',
+      caretaker: 'Jamie Street',
+      source: 'https://unsplash.com/photos/UtrE5DcgEyg',
+    },
+    {
+      id: 'post-2',
+      image: 'https://images.unsplash.com/photo-1450778869180-41d0601e046e?auto=format&fit=crop&w=850&q=80',
+      title: 'Woofs & friends',
+      description: 'Best friends are important for humans, but also for dogs.',
+      caretaker: 'Krista Mangulsone',
+      source: 'https://unsplash.com/photos/9gz3wfHr65U',
+    },
+    {
+      id: 'post-3',
+      image: 'https://images.unsplash.com/photo-1558947530-cbcf6e9aeeae?auto=format&fit=crop&w=634&q=80',
+      title: 'Good Woofs',
+      description: 'A good woof is a woof that brings joy. Here are a few tips to let your woof behave.',
+      caretaker: 'Olia Nayda',
+      source: 'https://unsplash.com/photos/f6v_Q0WXEK8',
+    },
+    {
+      id: 'post-4',
+      image: 'https://images.unsplash.com/photo-1444212477490-ca407925329e?auto=format&fit=crop&w=1100&q=80',
+      title: 'Wild Woofs',
+      description: 'In some parts of the world, wild woofs are very common. Learn how to interact in a nice way.',
+      caretaker: 'Anoir Chafik',
+      source: 'https://unsplash.com/photos/2_3c4dIFYFU',
+    },
+    {
+      id: 'post-5',
+      image: 'https://images.unsplash.com/photo-1567014543648-e4391c989aab?auto=format&fit=crop&w=1050&q=80',
+      title: 'Sleepy Woofs',
+      description: 'Sleeping is just as important for woofs as it is for humans. What are the main things your woof dreams about.',
+      caretaker: 'Max Singh',
+      source: 'https://unsplash.com/photos/2637Pic9xMw',
+    },
+    {
+      id: 'post-6',
+      image: 'https://images.unsplash.com/photo-1524511751214-b0a384dd9afe?auto=format&fit=crop&w=967&q=80',
+      title: 'Exploring Woofs',
+      description: 'Just sitting in one place is boring for most woofs. How do woofs explore the world?',
+      caretaker: 'Jamie Street',
+      source: 'https://unsplash.com/photos/wcO2PWLuQ3U',
+    },
+  ],
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# Home Screen
+
+This is the second React Native app that I have ever created.  It's the 'Woofstagram Home Screen' project with a few modifications from Codeacademy's React Native course.
